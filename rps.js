df3309caef77fc6e15f4fcb628973278c160d537
Ameeso <ameeso86@gmail.com>
2025-03-27 14:47:25 +0000

    one round of the rock paper scissors game works so far

--- rps.js
+++ rps.js
@@ -1,20 +1,98 @@
 console.log("Hello World")
 
-
+//random number gen
 let a = Math.random()
 console.log(a)
 
+//generate a computer choice
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    compChoice = choices.at(Math.floor(Math.random() * choices.length));
-    return compChoice;
+    let numChoice = Math.floor(Math.random() *3) +1;
+
+    if (numChoice === 1) {
+        return ("rock");
+    }
+    else if (numChoice === 2) {
+        return ("paper")
+    }
+
+    else if (numChoice === 3) {
+        return ("scissors")
+    }
 }
 
-function getPlayerChoice() {
+//record a player's choice from a prompt
+function getHumanChoice() {
     let playerInput = prompt("Pick rock, paper or scissors");
     let playerChoice = playerInput;
-    return playerInput;
+    return playerChoice;
 }
 
+//log the computer and player choice
 console.log(getComputerChoice());
-console.log(getPlayerChoice());
+console.log(getHumanChoice());
+
+let humanScore = 0
+let computerScore = 0
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+            return("It's a tie!")
+    }
+    
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore++;
+            return("You win!")
+        }
+        
+        else {
+            computerScore++;
+            return("Computer wins!")}
+    }
+
+    if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            humanScore++;
+            return("You win!")
+        }
+        else {
+            computerScore++;
+            return("Computer wins!")
+        }
+    }    
+    
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            humanScore++;
+            return("You win!")
+        }
+        else {
+            computerScore++;
+            return("Computer wins!")
+        }
+    }
+}
+
+console.log(humanScore)
+console.log(computerScore)
+
+
+function playGame() {
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
