c4eea03505382999d7b8d361c6113ee1a8316870
Ameeso <ameeso86@gmail.com>
2025-03-27 15:42:12 +0000

    finished the rock paper scissors game, and updated the README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rock-paper-scissors
+
+A Rock Paper Scissors, playable using browser prompts. The game is visible via the browser Console
+
--- rps.js
+++ rps.js
@@ -1,10 +1,11 @@
-console.log("Hello World")
+console.log("Welcome to Rock, Paper, Scissors!")
 
 //random number gen
 let a = Math.random()
-console.log(a)
+//console.log(a)
 
-//generate a computer choice
+//generate a random number, round it down (floor)
+//3 times (*3), and then add 1 to it
 function getComputerChoice() {
     let numChoice = Math.floor(Math.random() *3) +1;
 
@@ -20,21 +21,25 @@ function getComputerChoice() {
     }
 }
 
-//record a player's choice from a prompt
+//record input of player's choice from a prompt
+//then converts it to lowercase
+//assigns that input to the playerChoice variable
 function getHumanChoice() {
-    let playerInput = prompt("Pick rock, paper or scissors");
+    let playerInput = prompt("Pick rock, paper or scissors").toLowerCase();
     let playerChoice = playerInput;
     return playerChoice;
 }
 
-//log the computer and player choice
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+//log the computer and player choice. this was a test
+//console.log(getComputerChoice());
+//console.log(getHumanChoice());
 
+//create the score for human and computer, and set it to 0
 let humanScore = 0
 let computerScore = 0
 
-
+//this is the function for one round of the game
+//with each possible result
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
             return("It's a tie!")
@@ -74,19 +79,41 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-console.log(humanScore)
-console.log(computerScore)
+//console.log(humanScore)
+//console.log(computerScore)
 
-
+//function for the five rounds and score keeping, which calls the one round function
+//scores are set to 0, and a 'times' variable is created
+//'index' starts at 0. once the 'index' is more than 'times' = game played 5 times
+//then for loop stops
 function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+    const times = 5;
 
+    for (let index = 0; index < times; index++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        console.log(`Game ${index + 1}`);
+        console.log(`Human: ${humanChoice}`);
+        console.log(`Computer: ${computerChoice}`);
+        playRound(humanChoice, computerChoice);
+        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+        
+    }
 
-
-
-
-
+    if (humanScore > computerScore) {
+        console.log("Human wins the best of five!");
+    }    
+        else if (humanScore < computerScore) {
+            console.log("Computer wins the best of five!")
+        }
+        else {
+            console.log("It's a draw!")
+        }
 }
-
+//starts the game, which calls upon all the functions above
+playGame();
 
 
 
